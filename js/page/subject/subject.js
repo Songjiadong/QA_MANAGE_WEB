99686e8fb2f276dc854490fb73148f178c9fb49c
Songjiadong <songjiadong@miic.com.cn>
2019-03-14 15:18:33 +0800

    commit

--- js/page/subject/subject.js
+++ js/page/subject/subject.js
@@ -4,19 +4,25 @@ SubjectInfo.registerClass("SubjectInfo");
 SubjectInfo.Init = function init() {
     $("#sctMain").load(objPub.BaseUrl + "biz/subject.html", function (respones, status) {
         if (status == "success") {
-            $("#aAddCategroy").on("click", function () {
-                var itemStr = "";
-                itemStr += '<div class="category-item clear-fix">';
-                itemStr += '<div class="category-name">';
-                itemStr += '<input type="text" class="">';
-                itemStr += '</div>';
-                itemStr += '<div class="category-item-opts">';
-                itemStr += '<a href="javascript:;">取消</a>';
-                itemStr += '<a href="javascript:;">保存</a>';
-                itemStr += '</div>';
-                itemStr += '</div>';
-                $(".category-list").prepend(itemStr);
-            });
+            $("#aAddSubject").off("click").on("click", SubjectInfo.AddEvent);
+            $("#aAllRemoveSubject").off("click").on("click", SubjectInfo.AllRemoveEvent);
         }
     });
+}
+//删除所有主题事件
+SubjectInfo.AllRemoveEvent = function AllRemoveEvent(event) {
+}
+//新增主题事件
+SubjectInfo.AddEvent = function AddEvent(event) {
+    var itemStr = "";
+    itemStr += '<div class="category-item clear-fix">';
+    itemStr += '<div class="category-name">';
+    itemStr += '<input type="text" class="">';
+    itemStr += '</div>';
+    itemStr += '<div class="category-item-opts">';
+    itemStr += '<a href="javascript:;">取消</a>';
+    itemStr += '<a href="javascript:;">保存</a>';
+    itemStr += '</div>';
+    itemStr += '</div>';
+    $(".category-list").prepend(itemStr);
 }
--- js/page/tag/tag.js
+++ js/page/tag/tag.js
@@ -4,19 +4,28 @@ TagInfo.registerClass("TagInfo");
 TagInfo.Init = function init() {
     $("#sctMain").load(objPub.BaseUrl+"biz/tag.html", function (respones, status) {
         if (status == "success") {
-            $("#aAddCategroy").on("click", function () {
-                var itemStr = "";
-                itemStr += '<div class="category-item clear-fix">';
-                itemStr += '<div class="category-name">';
-                itemStr += '<input type="text" class="">';
-                itemStr += '</div>';
-                itemStr += '<div class="category-item-opts">';
-                itemStr += '<a href="javascript:;">取消</a>';
-                itemStr += '<a href="javascript:;">保存</a>';
-                itemStr += '</div>';
-                itemStr += '</div>';
-                $(".category-list").prepend(itemStr);
-            });
+            $("#aAddTag").off("click").on("click", TagInfo.AddEvent);
+            $("#aAllReomoveTag").off("click").on("click", TagInfo.AllRemoveEvent);
         }
     });
+}
+//新增标签
+TagInfo.AddEvent = function AddEvent(event) {
+    var itemStr = "";
+    itemStr += '<div class="category-item clear-fix">';
+    itemStr += '<div class="category-name">';
+    itemStr += '<input type="text" class="">';
+    itemStr += '</div>';
+    itemStr += '<div class="category-item-opts">';
+    itemStr += '<a href="javascript:;">取消</a>';
+    itemStr += '<a href="javascript:;">保存</a>';
+    itemStr += '</div>';
+    itemStr += '</div>';
+    $(".category-list").prepend(itemStr);
+}
+//删除所有标签事件
+TagInfo.AllRemoveEvent = function AllRemoveEvent(event) {
+}
+//删除标签事件
+TagInfo.DeleteEvent = function DeleteEvent(event) {
 }
